a8148d8e7e3dba3a93f89635144aaa352f7dc656
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-03-23 13:33:14 -0400

    Initialize empty app

---
+++ ApexPredators/ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Jeffrey Sweeney on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
